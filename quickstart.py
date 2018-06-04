d4c724b0c912e64154e8da69b1071904882bb832
Kyle Barron <barronk@mit.edu>
2018-06-04 14:58:12 -0400

    Update quickstart.py. - support only Python 3 - save credential to `~/.credentials/gmail-download.json`

--- quickstart.py
+++ quickstart.py
@@ -1,24 +1,19 @@
-
-from __future__ import print_function
 import httplib2
 import os
+import argparse
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = '/home/kyle/.config/gmail-download/client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
+APPLICATION_NAME = 'gmail-download'
 
 
 def get_credentials():
@@ -34,20 +29,17 @@ def get_credentials():
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+    credential_path = os.path.join(credential_dir, 'gmail-download.json')
 
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        credentials = tools.run_flow(flow, store, flags)
         print('Storing credentials to ' + credential_path)
     return credentials
+
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -65,9 +57,9 @@ def main():
     if not labels:
         print('No labels found.')
     else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
 
 
 if __name__ == '__main__':
