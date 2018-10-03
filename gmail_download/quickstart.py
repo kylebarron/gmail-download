c820ac2347290baf25450051f98a1dc2102e88ea
Kyle Barron <barronk@mit.edu>
2018-10-03 13:50:59 -0400

    Remove printing gmail labels from authentication flow

--- gmail_download/quickstart.py
+++ gmail_download/quickstart.py
@@ -53,16 +53,5 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-
 if __name__ == '__main__':
     main()
